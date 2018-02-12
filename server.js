4a7b9c11d7d560d3a4ee72d449d279b7b83d90c4
Bartek Buslowicz <bartek@burin.pl>
2018-02-12 13:48:32 +0100

    Add  final project

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('home-site');
+});
+
+app.get('/auth/google', function(req, res){
+	res.render('dest-site');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
